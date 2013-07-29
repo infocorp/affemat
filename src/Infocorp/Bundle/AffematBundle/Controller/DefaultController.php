f4c11d1039d8b05d8aea9b038ad4e8e9c5c05657
Eduardo Borges <eduardomrb@gmail.com>
2013-07-29 19:09:48 -0400

    Cria layout padrão do site, adiciona template do sonata page (affemat)

--- src/Infocorp/Bundle/AffematBundle/Controller/DefaultController.php
+++ src/Infocorp/Bundle/AffematBundle/Controller/DefaultController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('InfocorpAffematBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('InfocorpAffematBundle:Default:index.html.twig', array('name' => 'name'));
+    }
+
+    public function noticiasAction()
+    {
+        return $this->render('InfocorpAffematBundle:Default:index.html.twig', ['name' => 'Marcelon']);
     }
 }
