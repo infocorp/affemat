060e2306d02beadc620d1b71cf0549cfbc9643ca
Eduardo Borges <eduardomrb@gmail.com>
2013-08-12 14:38:37 -0400

    Adiciona GalleryController

---
+++ src/Infocorp/Bundle/AffematBundle/Controller/GalleryController.php
@@ -0,0 +1,15 @@
+<?php
+namespace Infocorp\Bundle\AffematBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class NewsController extends Controller
+{
+    public function indexAction()
+    {
+        
+    }
+}
