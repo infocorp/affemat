1cb5fc8490f0903152cc79dd19d0ea81d6303bbe
Eduardo Borges <eduardomrb@gmail.com>
2013-08-12 14:53:36 -0400

    Adiciona actions index e view ao GalleryController, rotas de galerias e view index

--- src/Infocorp/Bundle/AffematBundle/Controller/GalleryController.php
+++ src/Infocorp/Bundle/AffematBundle/Controller/GalleryController.php
@@ -6,10 +6,36 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class NewsController extends Controller
+class GalleryController extends Controller
 {
     public function indexAction()
     {
-        
+        $galleries = $this->get('sonata.media.manager.gallery')->findBy(array(
+            'enabled' => true
+        ));
+
+        if (!$galleries) {
+            throw new NotFoundHttpException('Nenhuma galeria foi encontrada');
+        }
+
+        return $this->render('InfocorpAffematBundle:Gallery:index.html.twig', array(
+            'galleries' => $galleries,
+        ));
+    }
+
+    public function viewAction($id)
+    {
+        $gallery = $this->get('sonata.media.manager.gallery')->findOneBy(array(
+            'id'      => $id,
+            'enabled' => true,
+        ));
+
+        if (!$gallery) {
+            throw new NotFoundHttpException('Não foi possível encontrar a galeria');
+        }
+
+        return $this->render('SonataMediaBundle:Gallery:view.html.twig', array(
+            'gallery'   => $gallery,
+        ));
     }
 }
