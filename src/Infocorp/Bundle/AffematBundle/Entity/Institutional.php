4896c4199abaa0be67b22a489cc137efcc89fa6d
EduardoMRB <eduardomrb@gmail.com>
2013-08-24 09:43:47 -0400

    Adiciona entidade e classe admin das paginas institucionais

---
+++ src/Infocorp/Bundle/AffematBundle/Entity/Institutional.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Infocorp\Bundle\AffematBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Institutional
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Infocorp\Bundle\AffematBundle\Entity\InstitutionalRepository")
+ */
+class Institutional
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="enabled", type="boolean")
+     */
+    private $enabled;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="raw_content", type="text")
+     */
+    private $rawContent;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="content_formatter", type="string", length=255)
+     */
+    private $contentFormatter;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Institutional
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param boolean $enabled
+     * @return Institutional
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+    
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return boolean 
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Institutional
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
